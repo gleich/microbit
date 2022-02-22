f677a25b07bf881d7a30ffd2d133e06a4f04cf95
Matt Gleich <git@mattglei.ch>
2022-02-21 23:57:40 -0500

    feat: roulette
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,52 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use microbit::{
-    hal::{prelude::*, Timer},
-    Board,
-};
+use microbit::{display::blocking::Display, hal::Timer, Board};
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::rtt_init_print;
+
+const PIXELS: [(usize, usize); 16] = [
+    (0, 0),
+    (0, 1),
+    (0, 2),
+    (0, 3),
+    (0, 4),
+    (1, 4),
+    (2, 4),
+    (3, 4),
+    (4, 4),
+    (4, 3),
+    (4, 2),
+    (4, 1),
+    (4, 0),
+    (3, 0),
+    (2, 0),
+    (1, 0),
+];
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
 
-    let mut board = Board::take().unwrap();
-
+    let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let mut leds = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
 
-    board.display_pins.col1.set_low().unwrap();
-    let mut row1 = board.display_pins.row1;
+    let mut last_led = (0, 0);
 
     loop {
-        row1.set_low().unwrap();
-        timer.delay_ms(1_000_u16);
-        rprintln!("Toggled LED");
-        row1.set_high().unwrap();
-        timer.delay_ms(1_000_u16);
+        for current_led in PIXELS.iter() {
+            leds[last_led.0][last_led.1] = 0;
+            leds[current_led.0][current_led.1] = 1;
+            display.show(&mut timer, leds, 30);
+            last_led = *current_led;
+        }
     }
 }
